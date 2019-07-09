0dd02214f1da8a9af42c39b49985be4f148af619
indika <uduup@Champ.aero>
2019-07-10 00:11:39 +0800

    delete Unnessary Files

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,9 @@
-// @material-ui/icons
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 import LibraryBooks from "@material-ui/icons/LibraryBooks";
 import BubbleChart from "@material-ui/icons/BubbleChart";
 import LocationOn from "@material-ui/icons/LocationOn";
 import Notifications from "@material-ui/icons/Notifications";
-// core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.jsx";
 import UserProfile from "views/UserProfile/UserProfile.jsx";
 import Customer from "ERP/Pages/CustomerPage.js";
@@ -17,15 +15,13 @@ import Typography from "views/Typography/Typography.jsx";
 import Icons from "views/Icons/Icons.jsx";
 import Maps from "views/Maps/Maps.jsx";
 import NotificationsPage from "views/Notifications/Notifications.jsx";
-import Admin from "layouts/Admin.jsx";
 
-// core components/views for RTL layout
+//This defines dashboard Menu Items
 
 const dashboardRoutes = [
   {
     path: "/dashboard",
     name: "Dashboard",
-    rtlName: "لوحة القيادة",
     icon: Dashboard,
     component: DashboardPage,
     layout: "/admin"
@@ -33,7 +29,6 @@ const dashboardRoutes = [
   {
     path: "/user",
     name: "User Profile",
-    rtlName: "ملف تعريفي للمستخدم",
     icon: Person,
     component: UserProfile,
     layout: "/admin"
@@ -41,7 +36,6 @@ const dashboardRoutes = [
   {
     path: "/table",
     name: "Table List",
-    rtlName: "قائمة الجدول",
     icon: "content_paste",
     component: TableList,
     layout: "/admin"
@@ -49,7 +43,6 @@ const dashboardRoutes = [
   {
     path: "/typography",
     name: "Typography",
-    rtlName: "طباعة",
     icon: LibraryBooks,
     component: Typography,
     layout: "/admin"
@@ -57,7 +50,6 @@ const dashboardRoutes = [
   {
     path: "/icons",
     name: "Icons",
-    rtlName: "الرموز",
     icon: BubbleChart,
     component: Icons,
     layout: "/admin"
@@ -65,7 +57,6 @@ const dashboardRoutes = [
   {
     path: "/maps",
     name: "Maps",
-    rtlName: "خرائط",
     icon: LocationOn,
     component: Maps,
     layout: "/admin"
@@ -73,7 +64,6 @@ const dashboardRoutes = [
   {
     path: "/notifications",
     name: "Notifications",
-    rtlName: "إخطارات",
     icon: Notifications,
     component: NotificationsPage,
     layout: "/admin"
@@ -81,7 +71,6 @@ const dashboardRoutes = [
   {
     path: "/customer",
     name: "Customer",
-    rtlName: "پشتیبانی از راست به چپ",
     icon: LibraryBooks,
     component: Customer,
     layout: "/admin"
@@ -89,7 +78,6 @@ const dashboardRoutes = [
   {
     path: "/productCat",
     name: "Product Category",
-    rtlName: "پشتیبانی از راست به چپ",
     icon: LibraryBooks,
     component: ProductCategory,
     layout: "/admin"
@@ -97,7 +85,6 @@ const dashboardRoutes = [
   {
     path: "/productA",
     name: "Product",
-    rtlName: "پشتیبانی از راست به چپ",
     icon: LibraryBooks,
     component: Product,
     layout: "/admin"
@@ -105,7 +92,6 @@ const dashboardRoutes = [
   {
     path: "/pos",
     name: "Point Of Sale",
-    rtlName: "پشتیبانی از راست به چپ",
     icon: LibraryBooks,
     component: POS,
     layout: "/admin"
