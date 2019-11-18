ceb05d8e8948068e00aad05f876a6ef48a725b44
indika <uduup@Champ.aero>
2019-11-18 18:17:02 +0800

    Add all the changes

--- src/ERP/properties/applicationProperties.js
+++ src/ERP/properties/applicationProperties.js
@@ -1,6 +1,6 @@
 export const API_URL = "https://coolbook-product-service.herokuapp.com";
 export const AUTH_URL =
-  "https://furnico-authentication-server.herokuapp.com:8789/oauth/token";
+  "https://furnico-authentication-server.herokuapp.com/oauth/token";
 export const PRODUCT_TYPE_LIST = ["Consumable", "Service", "Storable Product"];
 
 // export const API_URL = "https://coolbook-product-service.herokuapp.com";
